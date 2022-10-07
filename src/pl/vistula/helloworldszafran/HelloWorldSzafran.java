f8e3d499a07c50f9e4869c75015c9e97e329b8b6
PiotrSzafranEDU <pszafra1@stu.vistula.edu.pl>
2022-10-07 12:51:05 +0200

    Additional Lines

--- src/pl/vistula/helloworldszafran/HelloWorldSzafran.java
+++ src/pl/vistula/helloworldszafran/HelloWorldSzafran.java
@@ -3,5 +3,8 @@ package pl.vistula.helloworldszafran;
 public class HelloWorldSzafran {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Szafran");
+        System.out.println("It");
+        System.out.println("is");
+        System.out.println("Friday!");
     }
 }
