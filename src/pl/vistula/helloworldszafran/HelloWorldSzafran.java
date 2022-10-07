ae64a5795cf3ea4e731ccb7be40403d65eb1d559
PiotrSzafranEDU <pszafra1@stu.vistula.edu.pl>
2022-10-07 12:50:28 +0200

    First Commit

---
+++ src/pl/vistula/helloworldszafran/HelloWorldSzafran.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldszafran;
+
+public class HelloWorldSzafran {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Szafran");
+    }
+}
